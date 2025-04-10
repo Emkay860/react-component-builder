531f65c07a7a76bdff6688b0bc2326543e78a491
Majid Kareem <43646844+Emkay860@users.noreply.github.com>
2025-04-10 22:34:10 +0100

    Replace tailwind.config with initial values (remove Safelist)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: {
-    files: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    safelist: [
-      "px-3",
-      "py-1",
-      "bg-blue-500",
-      "text-white",
-      "appearance-none",
-      // any additional classes
-    ],
-  } as unknown as { files: string[]; safelist: string[] },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
